30fbe536b3447b0f6c99d5dd44cb18a9a77a5bf2
eltrapico2 <83182260+eltrapico2@users.noreply.github.com>
2022-10-28 19:27:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+let items = {
+  cart: []//(item: "Bag", price: 200)],
+          //()
+};
+
+function addItem() {
+  let input_item = document.getElementById("add_item").value;
+  let input_price = Number(document.getElementById("add_price").value);
+
+  //alert(input_item, input_price);
+  items.cart.push({item: input_item, price: input_price});
+  showCart();
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function showCart() {
+  let x = document.getElementById("items_in_cart");
+  let current = "";
+
+  if (items.cart == "") {
+    x.innerHTML = "<b>No items in cart!</b>";
+  }
+  else {
+    items.cart.forEach(function(show_item) {
+        current += "<br>Item: " + show_item.item + "<b> Price: " + show_item.price + "<br>";
+    });
+    x.innerHTML = current;
+    document.getElementById("add_item").value = "";
+    document.getElementById("add_price").value = "";
+  }
+showCart();
+
+}
+
+
